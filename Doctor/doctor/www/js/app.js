f5c0bc4bb4b521a7d1bcea87c977e28a05b31ed6
Kathryn Hodge <kahodge@vassar.edu>
2016-05-07 16:13:56 -0400

    Working on patients screen

--- Doctor/doctor/www/js/app.js
+++ Doctor/doctor/www/js/app.js
@@ -52,5 +52,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/patients');
 });
